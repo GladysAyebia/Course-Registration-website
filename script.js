c9cebf7d8108513e3e3f70e235763018bad2f777
GladysAyebia <138405838+GladysAyebia@users.noreply.github.com>
2024-09-02 00:10:04 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.getElementById("registerBtn").addEventListener("click", function() {
+    document.getElementById("registrationForm").style.display = "block";
+});
+
+
+
+// Optionally, you can add more JavaScript to handle form submission and validation.
