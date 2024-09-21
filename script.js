7201965508bb0d08eed3e32747e530e37343dc44
GladysAyebia <138405838+GladysAyebia@users.noreply.github.com>
2024-09-21 15:01:16 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("registerBtn").addEventListener("click", function() {
-    const registrationForm = document.getElementById("registrationForm");
-    if (registrationForm.style.display === "none" || registrationForm.style.display === "") {
-        registrationForm.style.display = "block";
-    } else {
-        registrationForm.style.display = "none";
-    }
-});
